3c854fcd0330935402a3b5fd5fc9f3c90f57801c
tanvibote <90523193+tanvibote@users.noreply.github.com>
2023-07-23 19:27:17 +0530

    Update postModel.js

--- backend/model/postModel.js
+++ backend/model/postModel.js
@@ -1,1 +1,31 @@
+const mongoose = require("mongoose");
+const userModel = require("./userModel");
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  paragraph: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: [],
+  },
+});
+
+module.exports = Post = mongoose.model("post", postSchema);
 
